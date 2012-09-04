4137c63c160b4dbd0715f1078d9add2ed30f2c80
HorsekitLin <horsekit1982@gmail.com>
2012-09-04 14:15:57 +0800

    first commit

--- app.js
+++ app.js
@@ -4,30 +4,39 @@
  */
 
 var express = require('express')
-  , routes = require('./routes')
-  , http = require('http')
-  , path = require('path');
-
-var app = express();
+    , routes = require('./routes')
+    , store = require('./routes/store');
+//var app = module.exports = express.createServer();
+var app  = express.createServer();
+// Configuration
 
 app.configure(function(){
-  app.set('port', process.env.PORT || 3000);
+    //set html template dir
   app.set('views', __dirname + '/views');
+  //set html engine
   app.set('view engine', 'jade');
-  app.use(express.favicon());
-  app.use(express.logger('dev'));
+  //set bodyPaser
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(app.router);
-  app.use(express.static(path.join(__dirname, 'public')));
+  app.use(express.static(__dirname + '/public'));
 });
 
 app.configure('development', function(){
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
+
+app.configure('production', function(){
   app.use(express.errorHandler());
 });
 
-app.get('/', routes.index);
+// Routes
 
-http.createServer(app).listen(app.get('port'), function(){
-  console.log("Express server listening on port " + app.get('port'));
+app.get('/', routes.index);
+app.post('/file_upload',store.home );
+//app.get('/',store.home);
+//console.log(store);
+app.listen(3000, function(){
+    //console.log(routes.index());
+  console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 });
