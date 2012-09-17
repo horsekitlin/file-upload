aaa404ba27850a0f527eb0d5ae9b7b8c3417a84e
HorsekitLin <horsekit1982@gmail.com>
2012-09-17 15:43:56 +0800

    Revert "aaa"
    This reverts commit 86ca9184ec6a63eb717fabbcb77654176bc29d72.

--- routes/index.js
+++ routes/index.js
@@ -22,7 +22,7 @@ exports.fileupload = function(req, res){
     var file=req.files.files;
     var tmp_path=file.path,
         upload_rs=false,
-        target_path="/data/epubreader/"+file.name;
+        target_path="/data/oxenote/"+file.name;
     
          fs.rename(tmp_path , target_path , function(err){
             if(!err){
@@ -38,5 +38,6 @@ exports.fileupload = function(req, res){
                     });               
                 }
             res.redirect('/reader?/data/epubreader/'+file.name);
+            
         });
     }
