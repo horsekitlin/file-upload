152f9155e303381ef1661aca108c5e6135fd1363
HorsekitLin <horsekit1982@gmail.com>
2012-09-20 09:36:06 +0800

    將mongodb移除 使用fs 完成顯示資料夾目錄檔案 檔案大小顯示四捨五入

--- public/javascripts/jquery.fileupload.js
+++ public/javascripts/jquery.fileupload.js
@@ -1,9 +1,9 @@
 $('#files').bind('change', function(){
+    var num = $(":input[type=file]")["0"].files.length
     var type = /(.zip|.ZIP)$/i;
-   $(":input[type=file]").val(function(err,filename){
-        if (!type.test(filename)) {
-           // $(":input[type=file]").val();
-       };
-    });
+    JSON.stringify($(":input[type=file]")["0"].files["0"]);
+    for(var i=0;i<num;i++){
+        $("#filelist").append("<div class='row-fluid show-grid'><div class='span4'>"+$(":input[type=file]")["0"].files[i].name+"</div><div class='span4'>"+$(":input[type=file]")["0"].files[i].type+"</div><div class='span4'>"+$(":input[type=file]")["0"].files[i].size+"</div></div>");
+        }
 });
 
--- routes/index.js
+++ routes/index.js
@@ -2,42 +2,72 @@
 /*
  * GET home page.
  */
-var mongoose = require( 'mongoose' );
-var db = require('../db.config/mongoDB.config.js');
-var file_info = db.file_info.model( 'file_info');
-var mongoose = require("mongoose");
+var fs = require('fs');
 exports.index = function(req, res){
     res.render('index', {});
 };
 
 exports.fileupload = function(req, res){
-    var i,
-    target_path,
-    mongo_insert,
-    upload_rs,
-    now= new Date();
-    console.log(now.toLocaleString());
-    var result=new Array();
-    var fs = require('fs');
-    var file=req.files.files;
-    var tmp_path=file.path,
-        upload_rs=false,
-        target_path="/data/oxenote/"+file.name;
-    
-         fs.rename(tmp_path , target_path , function(err){
-            if(!err){
-                var upload_rs=true;
-                new file_info({
-                    filename:file.name
-                    ,filesize:file.size
-                    ,filetype:file.type
-                    ,upload_type:upload_rs
-                    ,date:now.toLocaleTimeString()
-                    }).save(function( err, user, count ){
-                        var mongo_insert=true;
-                    });               
+    var stat,
+        now= new Date();
+    var arr = [];
+    var allStat = [];
+    fs.readdir("/data/epubreader/",function(err, files){
+        
+        for(var i=0 ; i<files.length ; i++){
+            stat=fs.statSync("/data/epubreader/"+files[i]);
+            stat.filename=files[i];
+            stat.size=Math.round(stat.size/1024);
+            allStat.push(stat);
+            }
+        if(req.files.files.length < 100){
+            for(var i=0;i<req.files.files.length;i++){
+                var temp=req.files.files[i];
+                var size=Math.round(temp.size/1024);
+                var tmp_path=temp.path,
+                    upload_rs=false,
+                    target_path="/data/epubreader/"+temp.name;
+                fs.rename(tmp_path , target_path , upload_rs= function(err){
+                    if(!err){
+                        var upload_rs=true;
+                        }
+                    else{
+                       console.log(err);
+                        }
+                        
+                    });
+                    arr.push({
+                        filename:temp.name
+                        ,filesize:size
+                        ,filetype:temp.type
+                        ,upload_type:upload_rs
+                        ,date:now.toLocaleTimeString()
+                            });      
                 }
-            res.redirect('/reader?/data/epubreader/'+file.name);
-            
-        });
+            }
+            else{
+                var temp=req.files.files;
+                var size=Math.round(temp.size/1024);
+                var tmp_path=temp.path,
+                    upload_rs=false,
+                    target_path="/data/epubreader/"+temp.name;
+                fs.rename(tmp_path , target_path , upload_rs= function(err){
+                    if(!err){
+                        var upload_rs=true;
+                        }
+                    else{
+                       console.log(err);
+                        }
+                        
+                    });
+                    arr.push({
+                        filename:temp.name
+                        ,filesize:size
+                        ,filetype:temp.type
+                        ,upload_type:upload_rs
+                        ,date:now.toLocaleTimeString()
+                    });
+                }
+                res.render("result",{file_info:arr,s_info:allStat})
+            });
     }
